6abb89b91ccaa28fd29d62b4b52036e9eb76e479
ROHAN SINGH BHOJAK <bhojakrohansingh@gmail.com>
2025-06-07 22:28:00 +0530

    Initial Commit

---
+++ LevelUpLife/LevelUpLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpLifeApp.swift
+//  LevelUpLife
+//
+//  Created by Rohan Bhojak  on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LevelUpLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
